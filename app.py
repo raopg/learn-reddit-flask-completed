252ca1a8570ede5e551123d2398418aca58b2333
Ryan Luu <ryanluu12345@gmail.com>
2018-11-13 20:06:55 -0800

    Added css styling

--- app.py
+++ app.py
@@ -9,9 +9,8 @@ app = Flask(__name__)
 def index():
     my_reddit_instance = reddit.create_reddit_instance()
     my_ten_hot_list = reddit.ten_top_titles(my_reddit_instance, 'UCI')
+    print(my_ten_hot_list)
     return render_template('index.html',posts = my_ten_hot_list,subreddit = 'UCI')
-
-
 
 if __name__ == "__main__":
     app.run(debug = True) #Set debug = False in a production environment
